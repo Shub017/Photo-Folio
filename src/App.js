5fa392f9ec67bdfc0079b177aaf1bd707b95c08e
Shub017 <153558542+Shub017@users.noreply.github.com>
2024-03-17 12:11:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import AlbumForm from './components/AlbumForm/AlbumForm';
 import AlbumList from "./components/AlbumList/AlbumList"; 
 
 function App() {
